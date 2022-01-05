e08374bebaecc8d6dadff87c3e31363c44015185
Namal Kulathunga <indika1@gmail.com>
2022-01-06 00:02:32 +1100

    Update timings

--- play.py
+++ play.py
@@ -2,15 +2,15 @@ import vlc
 import time
 import random
 
-p = vlc.MediaPlayer("file:///home/namal/MyTest/Python/pypasstheparcel/media/song1.mp3")
+p = vlc.MediaPlayer("file:///pypasstheparcel/media/song1.mp3")
 p.play()
 
 i = 0
 
-while i <= 6:
+while i <= 10:
     playtime = random.randint(20,50)
     p.play()
     time.sleep(playtime)
     p.stop()
-    time.sleep(30)
+    time.sleep(60)
     i += 1 
